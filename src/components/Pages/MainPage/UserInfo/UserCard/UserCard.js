426593cdbfa8346ee0d509490d605ba201bef6a2
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-08-31 02:35:12 +0200

    add more on cards

--- src/components/Pages/MainPage/UserInfo/UserCard/UserCard.js
+++ src/components/Pages/MainPage/UserInfo/UserCard/UserCard.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Card, Rating } from 'semantic-ui-react';
+import { Rating } from 'semantic-ui-react';
 import './UserCard.styles.scss';
 
 const UserCard = () => {
   return (
-    <Card className='user-card'>
+    <div className='user-card'>
       <h2>Level</h2>
       <div className='card-description'>
         <h3>Silver</h3>
         <Rating icon='star' defaultRating={2} maxRating={3} size='large' disabled />
       </div>
-    </Card>
+    </div>
   );
 };
 
